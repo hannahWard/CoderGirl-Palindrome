1127d5fd21eb86f2bd09e0887600cccda7b776cd
h.c.ward1991@gmail.com <h.c.ward1991@gmail.com>
2019-03-07 19:05:04 -0600

    Checking for palindromes

--- Palindrome/Program.cs
+++ Palindrome/Program.cs
@@ -6,9 +6,35 @@ namespace Palindrome
     {
         public static void Main()
         {
+            Console.WriteLine("Please, enter a word: ");
+            string userInput = Console.ReadLine();
+            char[] userInputArray = userInput.ToCharArray();
+            userInput = string.Join("", userInputArray);
+
+            Array.Reverse(userInputArray);
+            string reverseUserInput = string.Join("", userInputArray);
+
+            Console.WriteLine(CheckPalindrome(reverseUserInput, userInput));
             Console.ReadLine();
         }
 
         // TODO: Create a method that recognizes palindromes.
+
+        public static string CheckPalindrome(string reverseUserInput, string userInput)
+        {
+
+            string message = "";
+
+            if (reverseUserInput.Equals(userInput))
+            {
+                message = "true";
+            }
+            else
+            {
+                message = "false";
+            }
+            return message;
+
+        }
     }
 }
